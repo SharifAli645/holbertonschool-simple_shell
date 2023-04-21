3b11fb1f2bda5ae2c458371018d8678c1d8a0c11
SharifAli645 <pandemiaonirica@gmail.com>
2023-04-21 09:47:51 -0700

    feax: solution unused parameter and short functions

--- main.h
+++ main.h
@@ -1,6 +1,11 @@
 #ifndef ONE_H
 #define ONE_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
 #include <unistd.h>
-void eval_token(char *tkn, char *argv[], char *cmd, char *cmd_cpy, char *delim);
-int eval_fork(pid_t pid, char *cmd, char *cmd_cpy, char *argv[]);
+extern char **environ;
+char **eval_token(char *tkn, char *argv[], char *cmd, char *cmd_cpy, char *delim);
+int eval_fork(pid_t pid, char *cmd, char *cmd_cpy, char *argv[], char *ex);
 #endif
--- simple_shell.c
+++ simple_shell.c
@@ -1,22 +1,24 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/wait.h>
 /**
+ * eval_fork - function that receives the child and father
+ * @pid: pid
+ * @cmd: string
+ * @cmd_cpy: string
+ * @argv: argv
+ * @ex: binary
  *
- *
- *
+ * Return: e
  **/
-int eval_fork(pid_t pid, char *cmd, char *cmd_cpy, char *argv[])
+int eval_fork(pid_t pid, char *cmd, char *cmd_cpy, char *argv[], char *ex)
 {
 	if (pid == -1)
 		return (-1);
 	if (pid == 0)
 	{
-		int val = execve(argv[0], argv, NULL);
+		int val = execve(argv[0], argv, environ);
+
 		if (val == -1)
-			perror("Error");
+			perror(ex);
 	} else
 		wait(NULL);
 	free(cmd);
@@ -25,26 +27,58 @@ int eval_fork(pid_t pid, char *cmd, char *cmd_cpy, char *argv[])
 	return (1);
 }
 /**
+ * eval_token - function that
+ * @tkn: token
+ * @argv: argv
+ * @cmd: string
+ * @cmd_cpy: string
+ * @dl: delimitator
  *
- *
- *
+ * Return: pointer to array
  **/
-int main(void)
+char **eval_token(char *tkn, char *argv[], char *cmd, char *cmd_cpy, char *dl)
+{
+	int i = 0;
+	int argc = 0;
+
+	tkn = strtok(cmd, dl);
+	while (tkn)
+	{
+		argc++;
+		tkn = strtok(NULL, dl);
+	}
+	argv = malloc(sizeof(char *) * argc);
+	tkn = strtok(cmd_cpy, dl);
+	while (tkn)
+	{
+		argv[i] = tkn;
+		tkn = strtok(NULL, dl);
+		i++;
+	}
+	argv[i] = NULL;
+	return (argv);
+}
+/**
+ * main - main function
+ * @arc: arc
+ * @arv: arv
+ *
+ * Return: e
+ **/
+int main(int arc, char *arv[])
 {
 	char *cmd, *cmd_cpy, *token;
 	char *delim = " \n";
 	size_t n;
-	int argc, i;
 	char **argv;
-	
+	(void) arc;
+
 	while (1)
 	{
 		cmd = NULL;
 		cmd_cpy = NULL;
 		token = NULL;
 		argv = NULL;
-		i = 0;
-		argc = 0;
 		n = 0;
 
 	printf("#code-here$ ");
@@ -52,27 +86,8 @@ int main(void)
 		return (-1);
 
 	cmd_cpy = strdup(cmd);
-
-	token = strtok(cmd, delim);
-
-	while (token)
-	{
-		argc++;
-		token = strtok(NULL, delim);
-	}
-
-	argv = malloc(sizeof(char *) * argc);
-	
-	token = strtok(cmd_cpy, delim);
-
-	while (token)
-	{
-		argv[i] = token;
-		token = strtok(NULL, delim);
-		i++;
-	}
-	argv[i] = NULL;
-	eval_fork(fork(), cmd, cmd_cpy, argv);
+	argv = eval_token(token, argv, cmd, cmd_cpy, delim);
+	eval_fork(fork(), cmd, cmd_cpy, argv, arv[0]);
 	}
 	return (0);
 }
